73c8c694521193fcba8cb7b753a612f06ff31898
kasader <casada980@gmail.com>
2026-07-06 12:22:07 +0900

    Port tmux config to nix-declarative

--- modules/home/terminal/tmux.nix
+++ modules/home/terminal/tmux.nix
@@ -13,6 +13,26 @@ in
       mouse = true;
 
       keyMode = "vi";
+
+      extraConfig = ''
+        # Split panes with \ (horizontal) and - (vertical)
+        bind \\ split-window -h
+        bind - split-window -v
+        unbind '"'
+        unbind %
+
+        # Reload config
+        bind r source-file ${config.xdg.configHome}/tmux/tmux.conf
+
+        # Switch panes with Alt-arrow, no prefix
+        bind -n M-Left select-pane -L
+        bind -n M-Right select-pane -R
+        bind -n M-Up select-pane -U
+        bind -n M-Down select-pane -D
+
+        bind -n C-w choose-tree -Z -w
+        bind -n M-BSpace kill-pane
+      '';
     };
   };
 }
